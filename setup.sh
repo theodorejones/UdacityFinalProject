01de75b50eb0583dd8856d93685d465766529a69
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-17 11:54:23 -0400

    Revert "Revert "Update app.py""
    This reverts commit 2b87f21371a8928ef6ded4a2dd482e2775f54867.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DATABASE_URL="postgres://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e"
+echo $DATABASE_URL
+echo "setup.sh script executed successfully!"
