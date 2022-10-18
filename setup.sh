dc44f5e77fff03bf02564bef4f56a12ca045f829
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-18 09:29:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-export AUTH0_DOMAIN='fsnd-harsh-capstone.us.auth0.com'
+export AUTH0_DOMAIN='still-butterfly-7094.us.auth0.com'
 export ALGORITHMS='RS256'
-export API_AUDIENCE='cagency'
+export API_AUDIENCE='https://udacityfinal/'
 
 #heroku database url
 export DATABASE_URL='postgres://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e'
