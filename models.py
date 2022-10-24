c150bbfa46bc3b5da6a8ca5f64c40c3772920d90
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-24 14:29:47 -0400

    Update models.py

--- models.py
+++ models.py
@@ -10,8 +10,8 @@ Variable to execute app locally
 '''
 
 
-#DATABASE_PATH = os.environ['DATABASE_URL']
-DATABSE_PATH = 'postgres://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e'
+DATABASE_PATH = os.environ['DATABASE_URL']
+#DATABSE_PATH = 'postgres://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e'
 
 
 db = SQLAlchemy()
