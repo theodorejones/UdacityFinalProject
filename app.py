a201803bc83c5b5151f355b3bba3de81c7f9738f
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-26 14:48:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_app(test_config=None):
     # create and configure the app
 
     app = Flask(__name__)
+    db = SQLAlchemy(app)
     setup_db(app)
     CORS(app)
 
@@ -318,6 +319,7 @@ def create_app(test_config=None):
 
 
 app = create_app()
+
 app.app_context().push()
 
 if __name__ == '__main__':
