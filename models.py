fac4af87c9cef5b8fc4da8ef2a4bcc95952e4da8
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-27 09:28:37 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,6 +9,9 @@ from flask_sqlalchemy import SQLAlchemy
 DATABASE_PATH = 'postgresql://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e'
 
 
+db = SQLAlchemy()
+
+
 '''
 setup_db(app) binds a flask application and a SQLAlchemy service
 '''
@@ -17,9 +20,6 @@ setup_db(app) binds a flask application and a SQLAlchemy service
 def setup_db(app, database_path=DATABASE_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_PATH
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db = SQLAlchemy()
-    
     db.app = app
     db.init_app(app)
     db.create_all()
