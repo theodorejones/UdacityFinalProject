1d5af1e50048bfded74039632d7525ce7c00dfd8
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-27 07:57:24 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,9 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 DATABASE_PATH = 'postgresql://ulxxdhwkddjwqy:85e70942145391f2ffdb063db6bcc4e425659cdef7682654893e88d2a2c945c2@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d3u8jvplibk40e'
 
 
-db = SQLAlchemy()
-
-
 '''
 setup_db(app) binds a flask application and a SQLAlchemy service
 '''
