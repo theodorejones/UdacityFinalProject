ff942e3ff0ee313933ab86b9e262dfc0419e02ee
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-10-31 10:03:00 -0400

    Update models.py

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
-DATABASE_PATH = os.environ['DATABASE_URL']
-#DATABASE_PATH = os.getenv("DATABASE_URL")
+#DATABASE_PATH = os.environ['DATABASE_URL']
+DATABASE_PATH = os.getenv("DATABASE_URL")
 if DATABASE_PATH.startswith("postgres://"):
     DATABASE_PATH = DATABASE_PATH.replace("postgres://", "postgresql://", 1)
 
